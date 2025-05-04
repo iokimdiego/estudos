cbece4953ca5de0d626bcff1b4c0e2b0dad1f784
Iokim Diego <iokimdiego@hotmail.com>
2025-05-04 10:49:42 -0400

    feat(Aula 1): Apresenta 5 formas de selecionar elementos do DOM com JS

---
+++ front-end-com-bia/Aulas-Páginas-Dinâmicas/selectingElements.js
@@ -0,0 +1,17 @@
+const selecCustomTitle = document.getElementById("customTitle");
+
+console.log("Título Selecionado:", selecCustomTitle);
+
+const selectButtons = document.getElementsByTagName("button");
+
+console.log("Botões da Página:", selectButtons);
+
+const selectBoldTexts = document.getElementsByClassName("boldText");
+
+console.log("Textos em Negrito:", selectBoldTexts);
+
+const boldTexts = document.querySelector(".boldText");
+console.log("Bold Texts:", boldTexts);
+
+const allBoldTexts = document.querySelectorAll(".boldText");
+console.log("Bold Texts:", allBoldTexts);
