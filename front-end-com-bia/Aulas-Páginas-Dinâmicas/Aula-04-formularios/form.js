c8f696c7655d108fbae11f8d27df44c439414bad
Iokim Diego <iokimdiego@hotmail.com>
2025-05-06 16:05:27 -0400

    feat(Aula 04): Usa o JS para validar se algum checkbox foi marcado e manipula o DOM de acordo com o resultado

--- front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-04-formularios/form.js
+++ front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-04-formularios/form.js
@@ -1,6 +1,20 @@
 const form = document.querySelector('form');
+const geladinhos = document.querySelector('#geladinhos'); // Selecionando o fieldset 'id=geladinhos'
+const checkboxes = document.querySelectorAll('input[type="checkbox"]') // Selecionando todos o inputs do tipo checkbox
 
 form.addEventListener('submit', (evento) => {
+    const atLeastOneCheckboxMarked = Array.from(checkboxes).some(item => item.checked);
+    
     evento.preventDefault();
-    console.log("Enviando seus dados do formulário");
+    if(atLeastOneCheckboxMarked){
+        console.log("Enviando seus dados do formulário");
+        document.body.innerHTML = '<h1>Formulário Enviado com sucesso!</h1>';
+    } else {
+        const feedback = document.createElement('p');
+        feedback.innerText = 'preencha ao menos 1 checkbox';
+        feedback.style.color = 'red';
+        feedback.style.fontSize = '12px';
+        geladinhos.appendChild(feedback);
+    }
 })
+
