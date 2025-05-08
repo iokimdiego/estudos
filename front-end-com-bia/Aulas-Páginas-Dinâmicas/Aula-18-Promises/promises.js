88dbd127708653bde41b34226a224a7af038006a
Iokim Diego <iokimdiego@hotmail.com>
2025-05-08 16:07:49 -0400

    feat(Aula 18): Cria formas de capturar erros que são passados ou não pela API externa

--- front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-18-Promises/promises.js
+++ front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-18-Promises/promises.js
@@ -1,5 +1,5 @@
-const request = fetch('https://meowfacts.herokuapp.com/');
-console.log(request);
+// const request = fetch('https://meowfacts.herokuapp.com/');
+// console.log(request);
 
 // const getCatsFacts = () => {
 //     fetch('https://meowfacts.herokuapp.com/')
@@ -11,8 +11,19 @@ console.log(request);
 
 const getHarryPotterHouses = () => {
     fetch('https://potterapi-fedeperin.vercel.app/pt/houses')
-        .then((resposta) => resposta.json())
-        .then((harryPotterHouses) => console.log(harryPotterHouses));
+    .then((resposta) => {
+        if (!resposta.ok) {
+            console.log("A Promise foi rejeitada!");
+        }
+        return resposta.json()
+    })
+    .then((data) => {
+        if (data.error){
+            throw new Error(data.error);
+        }
+        console.log("Harry Potter Houses:", data)
+    })
+    .catch((err) => console.log(err));
 }
 
 getHarryPotterHouses();
