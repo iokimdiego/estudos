e2197912ed73fb1c993540b261a096ada9492dc5
Iokim Diego <iokimdiego@hotmail.com>
2025-05-08 16:22:52 -0400

    feat(Aula 19): Aplica o Async Await. Inicia o tratamento de erros com try/catch

--- front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-19-Async-Await/asyncAwait.js
+++ front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-19-Async-Await/asyncAwait.js
@@ -1,18 +1,28 @@
-const getHarryPotterHouses = () => {
-    fetch('https://potterapi-fedeperin.vercel.app/pt/houses')
-    .then((resposta) => {
-        if (!resposta.ok) {
-            console.log("A Promise foi rejeitada!");
-        }
-        return resposta.json()
-    })
-    .then((data) => {
-        if (data.error){
-            throw new Error(data.error);
-        }
-        console.log("Harry Potter Houses:", data)
-    })
-    .catch((err) => console.log(err));
+// const getHarryPotterHouses = () => {
+//     fetch('https://potterapi-fedeperin.vercel.app/pt/houses')
+//     .then((resposta) => {
+//         if (!resposta.ok) {
+//             console.log("A Promise foi rejeitada!");
+//         }
+//         return resposta.json()
+//     })
+//     .then((data) => {
+//         if (data.error){
+//             throw new Error(data.error);
+//         }
+//         console.log("Harry Potter Houses:", data)
+//     })
+//     .catch((err) => console.log(err));
+//}
+
+const getHarryPotterHouses = async () => {
+    try{
+        const resposta = await fetch('https://potterapi-fedeperin.vercel.app/ptt/houses');
+        const dados = await resposta.json();
+        console.log("Dados: ", dados);
+    } catch (err){
+        console.log(err);
+    }
 }
 
 getHarryPotterHouses();
