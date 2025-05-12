231c0eae2cdd74bcc86228a0e69746a0daa15a9f
Iokim Diego <iokimdiego@hotmail.com>
2025-05-12 09:54:15 -0400

    feat(Aula 19): Cria um bloco de instuções com Async Await para fazer uma consulta a uam API externa e prevenir erros

--- front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-19-Async-Await/asyncAwait.js
+++ front-end-com-bia/Aulas-Páginas-Dinâmicas/Aula-19-Async-Await/asyncAwait.js
@@ -17,8 +17,16 @@
 
 const getHarryPotterHouses = async () => {
     try{
-        const resposta = await fetch('https://potterapi-fedeperin.vercel.app/ptt/houses');
+        const resposta = await fetch('https://potterapi-fedeperin.vercel.app/pt/houses');
         const dados = await resposta.json();
+        if (dados.error){
+            throw new Error(dados.error);
+        }
+        if (!resposta.ok){
+            const status = resposta.status;
+            throw new Error('Ops! Algo deu errado!');
+            console.log('Status do Erro:', status);
+        }
         console.log("Dados: ", dados);
     } catch (err){
         console.log(err);
