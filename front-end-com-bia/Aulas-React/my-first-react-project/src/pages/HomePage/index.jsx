c630082f23749865a6c58fe5991c75f66fb78cef
Iokim Diego <iokimdiego@hotmail.com>
2025-05-25 11:14:59 -0400

    feat(Aula React): Renderização Condicional na página Homepage

--- front-end-com-bia/Aulas-React/my-first-react-project/src/pages/HomePage/index.jsx
+++ front-end-com-bia/Aulas-React/my-first-react-project/src/pages/HomePage/index.jsx
@@ -1,17 +1,33 @@
 
 import Button from "../../components/Button";
 
-const HomePage = () => (
-    <section id="homeSection">
-        <div id="photoHome"></div>
-        <div id="homeContents">
-            <div id="homeTexts">
-                <h1 class="titles">Iokim Diego</h1>
-                <h2>Desenvolvedor Full Stack</h2>
-                <Button buttonType="downloadResume"/>
-            </div>
-        </div>
-    </section>
-)
+const HomePage = () => {
+    const resumeDownloadComplete = true;
+    // const erro = true;
 
+    // if (erro){
+    //     return(
+    //         <div style={{paddingTop: '100px'}}>
+    //             <p>Erro</p>
+    //         </div>
+    //     )
+    // }
+
+    return (
+        <section id="homeSection">
+            <div id="photoHome"></div>
+            <div id="homeContents">
+                <div id="homeTexts">
+                    <h1 class="titles">Iokim Diego</h1>
+                    <h2>Desenvolvedor Full Stack</h2>
+                    {
+                        resumeDownloadComplete ? (
+                            <Button buttonType="downloadResume"/>
+                        ) : null
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
 export default HomePage;
