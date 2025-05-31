b122643ac09637f61a7d7c61dccf891e4c5f8065
Iokim Diego <iokimdiego@hotmail.com>
2025-05-30 20:04:02 -0400

    Add project files and initial build artifacts for OperadoresAritmeticos

---
+++ CSharpFundamentos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace OperadoresAritmeticos;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var a = 7;
+        var b = 3.0m;
+
+        Console.WriteLine(a + b); // Adição
+        Console.WriteLine(a - b); // Subtração
+        Console.WriteLine(a * b); // Multiplicação
+        Console.WriteLine(a / b); // Divisão
+        Console.WriteLine(a % b); // Módulo (resto da divisão)
+    }
+}
