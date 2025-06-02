b3ae655861d88421f80bc956b38a9c4d14d6b4ed
Iokim Diego <iokimdiego@hotmail.com>
2025-06-02 15:40:38 -0400

    Use switch case to ask the user for a option and print a message

--- CSharp/CSharpFundamentos/EstruturaCondicionalSwitch/Program.cs
+++ CSharp/CSharpFundamentos/EstruturaCondicionalSwitch/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Selecione a opção desejada: (1) Saldo, (2) Depósito, (3) Saque");
+        Console.WriteLine("Digite a opção:");
+        var opcao = Console.ReadLine();
+        var saldo = 100.00;
+        switch (opcao)
+        {
+            case "1":
+                Console.WriteLine($"Seu saldo é de R$ {saldo}");
+                break;
+            case "2":
+                Console.WriteLine("Informe o valor do depósito:");
+                var valorDeposito = Console.ReadLine();
+                Console.WriteLine($"Você depositou R$ {valorDeposito}");
+                saldo += Convert.ToDouble(valorDeposito);
+                Console.WriteLine($"Seu novo saldo é de R$ {saldo}");
+                break;
+            case "3":
+                Console.WriteLine("Informe o valor do saque:");
+                var valorSaque = Console.ReadLine();
+                Console.WriteLine($"Você sacou R$ {valorSaque}");
+                saldo -= Convert.ToDouble(valorSaque);
+                Console.WriteLine($"Seu novo saldo é de R$ {saldo}");
+                break;
+            default:
+                Console.WriteLine("Opção inválida.");
+                break;
+        }
     }
 }
--- CSharp/CSharpFundamentos/EstruturaCondicionalSwitch/obj/Debug/net9.0/EstruturaCondicionalSwitch.AssemblyInfo.cs
+++ CSharp/CSharpFundamentos/EstruturaCondicionalSwitch/obj/Debug/net9.0/EstruturaCondicionalSwitch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstruturaCondicionalSwitch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95fbf0beb3e370dd6905691e5b70ec2c8add2a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0197d5bbecc10dc11d40ab83f315d9351bc594")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstruturaCondicionalSwitch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstruturaCondicionalSwitch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
