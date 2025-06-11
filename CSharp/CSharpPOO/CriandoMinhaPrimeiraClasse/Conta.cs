ed2ef8dbbab304db0ba2e1ad3ecc73c73348ac77
Iokim Diego <iokimdiego@hotmail.com>
2025-06-11 15:03:16 -0400

    Aula de POO: Adicionando os métodos Depositar() e Sacar() passando parâmetros

--- CSharp/CSharpPOO/CriandoMinhaPrimeiraClasse/Conta.cs
+++ CSharp/CSharpPOO/CriandoMinhaPrimeiraClasse/Conta.cs
@@ -5,7 +5,7 @@ namespace Contas
     {
         // Atributos da classe Conta
         // Atributos são as características que definem a classe
-        public string Numero;
+        public string Numero = "";
         public DateTime DataAbertura;
         public decimal Saldo;
 
@@ -18,6 +18,32 @@ namespace Contas
             Console.WriteLine($"Data de Abertura:.........{DataAbertura.ToShortDateString()}");
             Console.WriteLine($"Saldo Atual:..............{Saldo:C}"); // Formata o saldo como moeda
             Console.WriteLine("===================================");
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("Valor do depósito deve ser maior que zero.");
+                return;
+            }
+            Saldo += valor;
+            Console.WriteLine($"Depósito de {valor:C} realizado com sucesso.");
+        }
+        public void Sacar(decimal valor)
+        {
+            if (valor <= 0)
+            {
+                Console.WriteLine("Valor do saque deve ser maior que zero.");
+                return;
+            }
+            if (valor > Saldo)
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+                return;
+            }
+            Saldo -= valor;
+            Console.WriteLine($"Saque de {valor:C} realizado com sucesso.");
         }
     }
 }
--- CSharp/CSharpPOO/CriandoMinhaPrimeiraClasse/Program.cs
+++ CSharp/CSharpPOO/CriandoMinhaPrimeiraClasse/Program.cs
@@ -12,5 +12,11 @@ class Program
         conta.Saldo = 0;
 
         conta.ExibirExtrato();
+        conta.Depositar(1000); // Deposita R$ 1000,00 chamando o método Depositar
+        conta.ExibirExtrato();
+        conta.Sacar(500); // Realiza um saque de R$ 500,00 chamando o método Sacar
+        conta.ExibirExtrato();
+        conta.Sacar(600); // Tenta sacar R$ 600,00, mas não tem saldo suficiente
+        conta.ExibirExtrato();
     }
 }
