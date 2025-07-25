1232842f425e149d3cfff64fcf098a06f2eca7de
Iokim Diego <iokimdiego@hotmail.com>
2025-07-25 11:24:56 -0400

    Aula de LINQ: Delegates Action, Func e Predicate

---
+++ CSharp/CSharpLINQ/CSharpLINQDelegates_Action_Func_Predicate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharpLINQDelegates_Action_Func_Predicate;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Action<string> exibir = ExibirInformacao;
+        exibir("Olá, Iokim!");
+
+        Func<int, int, int> realizarOperacao = Somar;
+        Console.WriteLine($"Resultado da soma: {realizarOperacao(5, 10)}");
+
+        Predicate<int> verificar = EhPar;
+        Console.WriteLine($"O número 1 é par? {verificar(1)}");
+    }
+
+    static void ExibirInformacao(string informacao)
+    {
+        Console.WriteLine($"Informação: {informacao}");
+    }
+
+    static int Somar(int a, int b)
+    {
+        return a + b;
+    }
+
+    static bool EhPar(int a)
+    {
+        return a % 2 == 0;
+    }  
+}
