656f27beb982977eab52b4580ee2421546447195
Iokim Diego <iokimdiego@hotmail.com>
2025-09-02 12:11:44 -0400

    Aula de PHP Fundamentos: Comentários de Linha e Comentários de Bloco

---
+++ PHP/PHPFundamentos/index.php
@@ -0,0 +1,24 @@
+<?php
+
+// Essa linha será completamente ignorada
+
+# Essa linha também será ignorada
+
+phpinfo(); // Isso será ignorado, mas o phpinfo() não!
+
+/*
+ *   Comentário de bloco
+ *
+ *   Por convenção, inicia-se cada linha com um *
+ *
+     mas não interfere em nada
+*/
+
+// Ctrl + ; é o atalho para comentário de linha
+
+/* Shift + Alt + A
+ * é o atalho para
+ * comentário de bloco   
+*/
+
+echo "Treinaweb";
