2dce47bbc0505885af9ced0f84d45b84461c3d17
Iokim Diego <iokimdiego@hotmail.com>
2025-09-02 14:47:44 -0400

    Aula de PHP Fundamentos: Cria a pasta operadores, os arquivos aritimeticos.php e comparadores.php

---
+++ PHP/PHPFundamentos/operadores/comparadores.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = 10;
+$b = 20;
+
+var_dump($a == $b); // A é igual a B?
+echo "<br>";
+
+var_dump($a != $b); // A é diferente de B?
+echo "<br>";
+
+var_dump($a <> $b); // A é diferente de B?
+echo "<br>";
+
+var_dump($a < $b); // A é menor que B?
+echo "<br>";
+
+var_dump($a > $b); // A é maior que B?
+echo "<br>";
+
+var_dump($a <= $b); // A é menor ou igual que B?
+echo "<br>";
+
+var_dump($a >= $b); // A é maior ou igual que B?
+echo "<br>";
