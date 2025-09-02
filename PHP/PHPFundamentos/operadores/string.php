114e0cfb90d332b8f1a3728c4319ef9ed5c6a965
Iokim Diego <iokimdiego@hotmail.com>
2025-09-02 15:20:52 -0400

    Aula de PHP Fundamentos: Declarando Strings com "" ou ''

--- PHP/PHPFundamentos/operadores/string.php
+++ PHP/PHPFundamentos/operadores/string.php
@@ -6,3 +6,19 @@ $curso = "PHP";
 $frase = "Faça o curso de " . $curso . " na escola " . $escola;
 
 echo $frase;
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+// Declaração de Strings com "" ou ''
+
+$string1 = "Faça o curso de $curso na escola $escola!"; // Com aspas duplas, interpreta as variáveis
+$string2 = 'Faça o curso de $curso na escola $escola!'; // Com aspas simples, tudo vira texto
+
+echo $string1;
+echo "<br>";
+echo $string2;
+
+// Por convenção, se coloca a variável entre {}, principalmente se nesse trecho de código será necessário uma interpretação mais complexa, exemplo uma array.
+// $string1 = "Faça o curso de {$curso[2]} na escola {$escola[3]}!";
