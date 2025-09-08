2ddabff460a3089a51af6ed5c2ff2aed328e7443
Iokim Diego <iokimdiego@hotmail.com>
2025-09-08 16:05:30 -0400

    Aula LINQ: Any, All e Contains

---
+++ CSharp/CSharpLINQ/Any-All-Contains/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Any_All_Contains;
+
+class Program
+{
+    private static List<int> notas = new List<int> { 500, 600, 700, 800, 900};
+    static void Main(string[] args)
+    {
+        // retornar true se algum elemento da coleção for maior que 700
+        var any = notas.Any(n => n > 700);
+        Console.WriteLine("Any: " + any);
+
+        // retornar true se todos os elementos da coleção forem maiores que 500
+        var all = notas.All(n => n > 500);
+        Console.WriteLine("All: " + all);
+
+        // retornar true se a coleção contiver o valor 900
+        var contains = notas.Contains(900);
+        Console.WriteLine("Contains: " + contains);
+    }
+}
