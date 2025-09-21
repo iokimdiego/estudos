51a505c6effea33954ffe3e537a3093be9115c32
Iokim Diego <iokimdiego@hotmail.com>
2025-09-21 09:25:37 -0400

    Aula de NuGet: Instalando o pacote Newtonsoft.Json

---
+++ CSharp/NuGet/ProjetoExemplo/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using ProjetoExemplo.models;
+
+var produto = new Produto
+{
+    Nome = "Notebook",
+    Valor = 2500m,
+    Estoque = 10
+};
+
+var json = JsonConvert.SerializeObject(produto);
+Console.WriteLine(json);
---
+++ CSharp/NuGet/ProjetoExemplo/models/Produto.cs
@@ -0,0 +1,9 @@
+namespace ProjetoExemplo.models;
+
+public class Produto
+{
+    public string Nome { get; set; } = string.Empty; // Inicializa com string vazia para evitar null
+    // public string? Nome { get; set; } // Permite null (usar se o nome puder ser nulo)
+    public decimal Valor { get; set; }
+    public int Estoque { get; set; }
+}
